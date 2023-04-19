e08d26dba0dccf547a9546896e25b23ef372d520
ThiagoDebossam <thiagodebossam8@gmail.com>
2023-04-18 23:01:43 -0300

    the init logic from users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Http\Requests\UserCreateRequest;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\Models\User
+     */
+    public function create(UserCreateRequest $request)
+    {
+        $data = $request->all();
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+}
---
+++ app/Http/Requests/UserCreateRequest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class UserCreateRequest extends FormRequest
+{
+
+    /**
+     * Indicates if the validator should stop on the first rule failure.
+     *
+     * @var bool
+     */
+    protected $stopOnFirstFailure = true;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+    
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|min:5|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|max:255',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O campo nome é obrigatório',
+            'email.required' => 'O campo e-mail é obrigatório',
+            'password.required' => 'O campo senha é obrigatório',
+            'name.min' => 'O campo nome deve conter no mínimo 5 caracteres',
+            'password.min' => 'O campo senha deve conter no mínimo 8 caracteres',
+            'name.max' => 'O campo nome deve conter no máximo 255 caracteres',
+            'password.max' => 'O campo senha deve conter no máximo 255 caracteres',
+            'email' => 'E-mail inválido',
+            'email.unique' => 'E-mail em uso, tente outro.',
+        ];
+    }
+
+    public function failedValidation(Validator $validator) 
+    { 
+        throw new HttpResponseException(response()->json([ 
+            'success' => false, 
+            'message' => 'Erros de validação', 
+            'data' => $validator->errors() 
+        ], 422)); 
+    }
+}
